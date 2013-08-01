401111811ed6abcbbdc826531c8248513cfb045a
lars.albrecht@gmail.com <lars.albrecht@gmail.com@402c8809-45d2-cbfe-dfbf-57c6e07cb8ec>
2013-08-01 14:32:56 +0000

    -> Added new Version: 	metadatabase as a library/framework 	jmoviedb as a concrete product

---
+++ trunk/src/com/lars_albrecht/mdb/main/MDB.java
@@ -0,0 +1,137 @@
+/**
+ * 
+ */
+package com.lars_albrecht.mdb.main;
+
+import com.lars_albrecht.general.utilities.Debug;
+import com.lars_albrecht.general.utilities.RessourceBundleEx;
+import com.lars_albrecht.mdb.main.core.controller.CollectorController;
+import com.lars_albrecht.mdb.main.core.controller.ExportController;
+import com.lars_albrecht.mdb.main.core.controller.FinderController;
+import com.lars_albrecht.mdb.main.core.controller.InterfaceController;
+import com.lars_albrecht.mdb.main.core.controller.MainController;
+import com.lars_albrecht.mdb.main.core.controller.TypeController;
+import com.lars_albrecht.mdb.main.core.exporter.PDFExport;
+import com.lars_albrecht.mdb.main.core.interfaces.SystemTrayInterface;
+import com.lars_albrecht.mdb.main.core.interfaces.TelnetInterface;
+import com.lars_albrecht.mdb.main.core.interfaces.WebInterface;
+import com.lars_albrecht.mdb.main.database.DB;
+
+/**
+ * @author lalbrecht
+ * 
+ */
+public class MDB {
+
+	private MainController		mainController	= null;
+	private MDBConfig			mdbConfig		= null;
+
+	private FinderController	fController		= null;
+	private TypeController		tController		= null;
+	private CollectorController	cController		= null;
+	private InterfaceController	iController		= null;
+	private ExportController	eController		= null;
+
+	public MDB(final MDBConfig mdbConfig) throws Exception {
+		if (mdbConfig != null) {
+			this.mdbConfig = mdbConfig;
+
+			this.init();
+		} else {
+			throw new Exception("No config for mdb or null");
+		}
+	}
+
+	public void run() {
+		final String ressourceBundleName = this.mdbConfig.getRessourceBundleName();
+
+		Debug.log(Debug.LEVEL_INFO, RessourceBundleEx.getInstance(ressourceBundleName).getProperty("application.name") + " ("
+				+ RessourceBundleEx.getInstance(ressourceBundleName).getProperty("application.version") + ")");
+
+		this.mainController.run();
+	}
+
+	private void init() {
+		Debug.loglevel = this.mdbConfig.getLoglevel();
+		RessourceBundleEx.setPrefix(this.mdbConfig.getRessourceBundleName());
+
+		this.initDB();
+
+		this.mainController = new MainController();
+
+		// Initialize controllers
+		this.initTypeController();
+		this.initFinderController();
+		this.initExporterController();
+		this.initInterfaceController();
+		this.initCollectorController();
+
+		// Set initialized Controllers
+		this.mainController.settController(this.tController);
+		this.mainController.setfController(this.fController);
+		this.mainController.seteController(this.eController);
+		this.mainController.setiController(this.iController);
+		this.mainController.setcController(this.cController);
+	}
+
+	private void initTypeController() {
+		this.tController = new TypeController(this.mainController);
+		this.tController.setTypers(this.mdbConfig.getListOfTypers());
+	}
+
+	private void initFinderController() {
+		this.fController = new FinderController(this.mainController);
+		this.fController.setFileFilter(this.mdbConfig.getFinderFileFilter());
+	}
+
+	private void initExporterController() {
+		this.eController = new ExportController(this.mainController);
+
+		// TODO TMP only, make it configurable (enable/disable/plugins)
+		this.mdbConfig.getListOfExporter().add(new PDFExport());
+
+		this.eController.setExporters(this.mdbConfig.getListOfExporter());
+	}
+
+	private void initInterfaceController() {
+		try {
+
+			this.iController = new InterfaceController(this.mainController);
+
+			// TODO TMP only, make it configurable (enable/disable/plugins)
+			if (this.mdbConfig.getSystemTrayInterfaceIconImageFile() != null) {
+				final SystemTrayInterface systemTrayInterface = new SystemTrayInterface();
+				systemTrayInterface.setTrayIconImagePath(this.mdbConfig.getSystemTrayInterfaceIconImageFile());
+				this.mdbConfig.getListOfInterfaces().add(systemTrayInterface);
+			}
+			this.mdbConfig.getListOfInterfaces().add(new TelnetInterface());
+
+			if (this.mdbConfig.getWebInterfaceFileDetailsOutputItem() != null) {
+				final WebInterface webInterface = new WebInterface();
+				webInterface.setFileDetailsOutputItem(this.mdbConfig.getWebInterfaceFileDetailsOutputItem());
+				this.mdbConfig.getListOfInterfaces().add(webInterface);
+			}
+
+			this.iController.setInterfaces(this.mdbConfig.getListOfInterfaces());
+
+		} catch (final Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void initCollectorController() {
+		this.cController = new CollectorController(this.mainController);
+		this.cController.setCollectors(this.mdbConfig.getListOfCollectors());
+	}
+
+	private void initDB() {
+		try {
+			new DB().init();
+		} catch (final Exception e) {
+			e.printStackTrace();
+			System.err.println("SYSTEM SHUT DOWN");
+			System.exit(-1);
+		}
+	}
+
+}
